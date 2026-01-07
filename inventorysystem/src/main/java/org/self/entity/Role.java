928a67d3ec61567a83c83140fb68a416758817c5
Pratik0444 <pratikkulkarni699@gmail.com>
2026-01-07 19:25:14 +0530

    role User commit

---
+++ inventorysystem/src/main/java/org/self/entity/Role.java
@@ -0,0 +1,6 @@
+package org.self.entity;
+
+public enum Role {
+  ADMIN,
+  USER
+}
---
+++ inventorysystem/src/main/java/org/self/entity/User.java
@@ -0,0 +1,29 @@
+package org.self.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="users")
+public class User {
+ @Id
+ @GeneratedValue(strategy = GenerationType.IDENTITY)
+ private long id;
+ 
+ @Column(nullable = false)
+ private String name;
+ 
+ @Column(nullable = false, unique = true)
+ private String email;
+ 
+ @Column(nullable = false)
+ private String password;
+ 
+ private Role role;
+}
